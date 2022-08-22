e785d31e993587c06fff255f0462411bac9639c5
greenmac <alwaysmac@msn.com>
2022-08-22 15:09:53 +0800

    api test

---
+++ README.md
@@ -0,0 +1,13 @@
+# 指令順序
+
+```
+pip3 install -r requirements.txt
+```
+
+```
+python3 -m gunicorn main:app 
+```
+
+### CLICK
+
+http://0.0.0.0:5000
---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from const_data import obj
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    data = json.dumps(obj, ensure_ascii=False)
+    print(type(obj))
+    return data
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
